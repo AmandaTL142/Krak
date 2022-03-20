c248fac3a4ebc5e8ab17e7dae7ea78991fc80ef0
andreajiao <andreajiao@gmail.com>
2022-03-20 07:41:06 +0100

    fjernet connection til lokal DB i application properties

--- src/main/java/backend/repository/HobbyRepo.java
+++ src/main/java/backend/repository/HobbyRepo.java
@@ -1,13 +1,7 @@
 package backend.repository;
 
-import backend.ConnectionManager;
 import backend.entity.Hobby;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public interface HobbyRepo extends JpaRepository<Hobby, String> {
 
